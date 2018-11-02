039a0ed4b3c87e070d51b6b1a72748b75e49dedc
Chris Aquino <chris.aquino@gmail.com>
2018-11-02 15:03:06 -0400

    adds randomization galore

--- index.js
+++ index.js
@@ -1,29 +1,119 @@
 const nerds = require('nerds');
+const {spawn} = require('child_process');
 
+const painSounds = [
+    'owwwchee',
+    'oof',
+    `it's only a flesh wound`,
+    'you punk. that hurt'
+];
 
 // grabs random harry potter character
+function getRandomHarryPotter() {
+    let harryPotterArray = nerds.resolve('Harry Potter').asArray();
+    let hpCharacter = harryPotterArray[0];
+    hpCharacter.name = `${hpCharacter.first} ${hpCharacter.last}`;
+    return hpCharacter;
+}
 // grabs random star wars character
-
-// transforms them into fighting characters
-// - add on an `attack` value
-// - add on a `health` value
+function getRandomStarWars() {
+    let starWarsArray = nerds.resolve('Star Wars').asArray();
+    return starWarsArray[0];
+}
 
 // copy and paste that random number function from mdn
+function getRandomInt(max=100, min=50) {
+    return Math.floor(Math.random() * (max - min) + min);
+    // return Math.floor(Math.random() * Math.floor(max));
+}
+
+// transforms them into fighting characters
+function makeFighter(character){
+    character.power = getRandomInt(100);
+    // - add on an `attack` value
+    character.health = getRandomInt(1000, 500);
+    // - add on a `health` value
+
+    // character.isMale = () => character.gender === 'Male';
+    character.isFemale = () => character.gender === 'Female';
+
+    character.yell = (what) => {
+        let voice;
+        if (character.isFemale()) {
+            voice = 'Victoria';
+        } else {
+            voice = 'Fred';
+        }        
+        spawn('say', ['-v', voice, what]);
+    }
+
+    character.attack = (enemy) => {
+        let howMuch = getRandomInt(character.power, character.power/2);
+        enemy.health = enemy.health - howMuch;
+        // spawn('say', ['owwwwwchee']);
+        enemy.yell(painSounds[getRandomInt(painSounds.length, 0)]);
+        character.lastDamage = howMuch;
+    };
+
+    character.isAlive = () => {
+        let stillKicking = character.health > 0;
+        if (!stillKicking) {
+            // spawn('say', ['oh no i am dead']);
+            character.yell('oh no i am dead');
+        }
+        return stillKicking;
+    };
+
+    return character;
+}
+
 
 // write a function that accepts two characters
 // and has one attack the other
 // and vice versa
 
-// check their health
-// run until there is one survivor
+function battle(f1, f2) {
+    console.log('================================');
+    console.log(`${f1.name}: ${f1.health}`);
+    console.log(`${f2.name}: ${f2.health}`);
+    console.log('--------------------------------');
+    f1.attack(f2);
+    console.log(`${f1.name} attacks ${f2.name} for ${f1.lastDamage} damage!`);
+    console.log('================================');
+    
+}
+
+function toTheDeath() {
+    let sw = makeFighter(getRandomStarWars());
+    let hp = makeFighter(getRandomHarryPotter());
+    let fighters = [sw, hp];
+    let battleInterval = setInterval(() => {
+        [sw, hp] = [hp, sw]; // swaps the items in the array.
+        battle(sw, hp);
+        if (!sw.isAlive() || !hp.isAlive()) {
+            clearInterval(battleInterval);
+        }
+    }, 1500);
+    // run until there is one survivor
+}
+
+toTheDeath();
+
+// let starWarsArray = nerds.resolve('Star Wars').asArray();
+
+// for (let i=0; i<harryPotterArray.length; i++) {
+//     let hpCharacter = harryPotterArray[i];
+//     let swCharacter = starWarsArray[i];
+//     // console.log(swCharacter);
+//     console.log(`${hpCharacter.first} ${hpCharacter.last} punches ${swCharacter.name} in the neck.`);
+// }
 
 
-let harryPotterArray = nerds.resolve('Harry Potter').asArray();
-let starWarsArray = nerds.resolve('Star Wars').asArray();
 
-for (let i=0; i<harryPotterArray.length; i++) {
-    let hpCharacter = harryPotterArray[i];
-    let swCharacter = starWarsArray[i];
-    // console.log(swCharacter);
-    console.log(`${hpCharacter.first} ${hpCharacter.last} punches ${swCharacter.name} in the neck.`);
+
+module.exports = {
+    getRandomHarryPotter,
+    getRandomStarWars,
+    // makeFighter,
+    toTheDeath
 }
